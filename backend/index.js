ab9f3827c17add081bc0a2f92452af4824db4333
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-13 13:48:42 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", mainRouter);
 
-app.get("/", function(req,res){
-    console.log("reaching http://localhost:3000/")
-})
-
-
-
-
 
 app.listen(3000, ()=>{
     console.log("Server listening on port : 3000");
